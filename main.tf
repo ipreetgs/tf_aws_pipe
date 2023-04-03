a290b3c6212e9507ae2d567d9fc9c8afd910554e
gsingh <ipreetgs.gmail.com>
2023-04-03 14:27:48 +0530

    tf

--- main.tf
+++ main.tf
@@ -6,15 +6,3 @@ resource "aws_s3_bucket" "example_bucket" {
   bucket = "demotxchd"
   acl    = "private"
 }
-
-# EC2 Instance resource
-resource "aws_instance" "web" {
-  ami           = "ami-00c39f71452c08778"
-  instance_type = "t2.micro"
-  key_name      = "tf"
-  security_groups = sg-06138044ec110f4ba
-  ebs_block_device {
-    device_name = "/dev/sdh"
-    volume_size = 10
-  }
-}
